55b2979f1e6c72ca8faefeca8d0273423fa0fc33
Jason2865 <jiashen030816@gmail.com>
2026-05-25 04:33:43 +0800

    Edit prompt message style, delete sidebar mystudent, layout design

--- Student Information Management System (SIMS)/Lecturer/Notifications.aspx.designer.cs
+++ Student Information Management System (SIMS)/Lecturer/Notifications.aspx.designer.cs
@@ -139,5 +139,41 @@ namespace Student_Information_Management_System__SIMS_.Lecturer
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Panel pnlEmpty;
+
+        /// <summary>
+        /// hfReadTarget control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hfReadTarget;
+
+        /// <summary>
+        /// hfDeleteTarget control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hfDeleteTarget;
+
+        /// <summary>
+        /// btnReadConfirmed control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnReadConfirmed;
+
+        /// <summary>
+        /// btnDeleteConfirmed control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDeleteConfirmed;
     }
 }
